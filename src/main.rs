636b8786a3433013375af0570d2f782f3edb36ae
Parshwa52 <parshwashahsbmp52@gmail.com>
2022-01-19 20:20:04 +0530

    Match statement done

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,9 @@
 fn main() {
     
-    let mut x = 1;
-    loop{
-        x*=2;
-        if x>5000
-        {break;}
-        println!("value of x is {}",x);
-    }
-
-    let mut y = 1;
-    while y < 5000
-    {
-        y*=2;
-        println!("value of y is {}",y);
-    }
-
-    for i in 0..5
-    {
-        println!("I = {}",i);
-    }
-
-    for j in 0..=5
-    {
-        println!("J = {}",j);
-    }
-
-    let arr =[7,8,9];
-
-    for val in arr
-    {
-        println!("value = {}",val);
+    let x =1;
+    match x{
+        1 => println!("One"),
+        2 => println!("Two"),
+        _ => println!("Wrong")
     }
 }
