1735db3aed83b84bd6b6cf2212eabd2ea71a9e14
Parshwa52 <parshwashahsbmp52@gmail.com>
2022-01-19 19:48:33 +0530

    Values and tuples and destructuring

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 fn main() {
     println!("Hello, world!");
-    let mut x =4;
-    println!("Value of x is {}",x);
-    x=5;
-    println!("Value of x is {}",x);
+    
+    let a = (1,"Hello world",false);
+    println!("The values are {} {}",a.0,a.1);
+
+    let val3 = a.2;
+    println!("Third value is {}",val3);
+
+    //destructuring
+
+    let (val1,val2,_) = a;
+    println!("The values are {} {}",val1,val2);
 }
